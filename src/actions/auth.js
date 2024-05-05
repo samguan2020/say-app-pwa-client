c4738b28a0e15678913c3e3c37cc5e740aa302b9
Xin Guan <xin.guan@gmail.com>
2024-05-04 20:48:28 -0700

    fix manifest and signup auto signin issue

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -17,7 +17,7 @@ export const signup = (formData, router) => async (dispatch) => {
   try {
     const { data } = await api.signUp(formData);
 
-    dispatch({ type: AUTH, data });
+    // dispatch({ type: AUTH, data });
 
     router.push('/');
   } catch (error) {
